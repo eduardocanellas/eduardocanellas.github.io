b8dfb9ba4e98b7fb4c9970b3f00ff37bea556f44
Eduardo Canellas de Oliveira <eduardocanellas@id.uff.br>
2019-11-20 12:49:49 -0300

    projeto inicializado em react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to the Canellas web page!</h1>
+      <p>At this moment, the site is under construction =(</p>
+      <p>Gradually, I will update it to become more interesting ;)</p>
+      <br />
+      <address>
+        <p>My contact information:</p>
+        <ul>
+          <li style={{listStyle: "none"}}>eduardocanellas@id.uff.br</li>
+        </ul>
+      </address>
+    </>
+  );
+}
+
+export default App;
