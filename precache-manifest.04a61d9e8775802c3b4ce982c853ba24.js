2e5dcf5ae8488c3cf386b9408b58cb88eb67f078
Eduardo Canellas de Oliveira <eduardocanellas@id.uff.br>
2019-11-21 00:37:39 +0000

    Updates

---
+++ precache-manifest.04a61d9e8775802c3b4ce982c853ba24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a92ca29627e821b82053523d21208c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e211f1dae74359e823d7",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9ed031cf487fe1e9a597",
+    "url": "/static/js/2.fdcbcd73.chunk.js"
+  },
+  {
+    "revision": "e211f1dae74359e823d7",
+    "url": "/static/js/main.30691e2c.chunk.js"
+  },
+  {
+    "revision": "66ec99c66fc63932591b",
+    "url": "/static/js/runtime-main.0cc5ba3c.js"
+  }
+]);
